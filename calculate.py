b57db3262d83839b117debf1dde6d4d0fc7111a4
Mansiundre <156295332+Mansiundre@users.noreply.github.com>
2024-10-06 21:12:17 -0700

    Update fum2.py

--- calculate.py
+++ calculate.py
@@ -1,84 +1,16 @@
-class MathOp:
-
-                def AddOp(self):
-
-                                self.a=int(input("Enter first no:"))
-
-                                self.b=int(input("Enter Second no:"))
-
-                                self.c= self.a + self.b
-
-                                print("Addition is:",self.c)
-
- 
-
-                def SubOp(self):
-
-                                self.a=int(input("Enter first no:"))
-
-                                self.b=int(input("Enter Second no:"))
-
-                                self.c= self.a - self.b
-
-                                print("Sub is:",self.c)
+def factorial(n):
+    if n < 0:
+        return 0
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        fact = 1
+        while(n > 1):
+            fact *= n
+            n -= 1
+        return fact
 
 
-
-                def MulOp(self):
-
-                                self.a=int(input("Enter first no:"))
-
-                                self.b=int(input("Enter Second no:"))
-
-                                self.c= self.a * self.b
-
-                                print("Addition is:",self.c)
-
-                                print("Multiplication is:",self.c)
-
-#main body
-
-
-
-obj=MathOp()
-
-
-
-while True:
-
-                print("\n1. Addtion")
-
-                print("2. Substraction")
-
-                print("3. Multiplication")
-
-                print("4. Exit")
-
- 
-
-                ch=int(input("Enter choice to perform any opertaion"))
-
-                if ch==1:
-
-                                obj.AddOp()
-
-                elif ch==2:
-
-                                obj.SubOp()
-
-                elif ch==3:
-
-                                obj.MulOp()
-
-                elif ch==4:
-
-                                print("\nProgram Stop")
-
-                                break
-
-                else:
-
-                                print("Wrong Choice")
-
-
-
+num = 5
+print("Factorial of",num,"is",
+factorial(num))
